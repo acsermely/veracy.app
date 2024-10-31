77109f308319b3fa20e50cdc293bef9517fc9484
Attila Csermely <csermelyati@gmail.com>
2024-10-31 01:59:37 +0100

    PWA init

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { svelte } from "@sveltejs/vite-plugin-svelte";
 import path from "path";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
-  resolve: {
-    alias: {
-      $lib: path.resolve("./src/lib"),
-    },
-  },
-})
+	plugins: [svelte(), VitePWA({ registerType: "autoUpdate" })],
+	resolve: {
+		alias: {
+			$lib: path.resolve("./src/lib"),
+		},
+	},
+});
