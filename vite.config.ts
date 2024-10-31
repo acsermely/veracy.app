edbabba07e64592b62ba9ecf244421571c590fdd
Attila Csermely <csermelyati@gmail.com>
2024-10-31 13:13:00 +0100

    extend PWA manifest

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,28 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [svelte(), VitePWA({ registerType: "autoUpdate" })],
+	plugins: [
+		svelte(),
+		VitePWA({
+			registerType: "autoUpdate",
+			manifest: {
+				name: "Veracy",
+				short_name: "Veracy",
+				description:
+					"Veracy is The decentralized social media platform.",
+				theme_color: "#000000",
+				display: "standalone",
+				icons: [
+					{
+						src: "veracy-icon.svg",
+						type: "image/svg+xml",
+						sizes: "any",
+						purpose: "monochrome any",
+					},
+				],
+			},
+		}),
+	],
 	resolve: {
 		alias: {
 			$lib: path.resolve("./src/lib"),
